e528cc1413bd11ef0768b3b8bfbe36818da43070
testmelih <testmelihinci@gmail.com>
2023-04-25 14:53:06 +0000

    main

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-// Get the ring circle element
+// Get the ring circle and inner circle elements
 var ringCircle = document.getElementById("ringCircle");
+var innerCircle = document.getElementById("innerCircle");
+
+// Get the size range input element
+var sizeRange = document.getElementById("sizeRange");
+
+// Add event listener for when the size range changes
+sizeRange.addEventListener("input", function(event) {
+    // Get the value of the size range
+    var sizeValue = event.target.value;
+    
+    // Update the inner circle size based on the size range value
+    innerCircle.style.width = sizeValue + "%";
+    innerCircle.style.height = sizeValue + "%";
+    
+    // Update the ring size text
+    document.getElementById("ringSize").textContent = "Ring Size: " + sizeValue;
+});
 
 // Add event listener for when the ring circle is clicked
 ringCircle.addEventListener("click", function(event) {
-    // Get the size of the ring circle
-    var ringSize = getRingSize(event.offsetX, event.offsetY);
+    // Get the size of the ring from the size range
+    var ringSize = sizeRange.value;
     
     // Update the ring size text
     document.getElementById("ringSize").textContent = "Ring Size: " + ringSize;
 });
-
-// Function to calculate the ring size based on the clicked position
-function getRingSize(x, y) {
-    // Calculate the distance from the center of the circle
-    var distance = Math.sqrt(Math.pow(x - 150, 2) + Math.pow(y - 150, 2));
-    
-    // Adjust the distance to match the desired ring size range
-    var ringSize = 1 + Math.floor(distance / 20);
-    
-    return ringSize;
-}
